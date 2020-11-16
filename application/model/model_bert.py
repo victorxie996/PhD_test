6360fb46ff3361b92c43642d601758f902e92cb8
Victor Xie <56447006+victorxie996@users.noreply.github.com>
2020-11-16 15:30:00 +0000

    Add files via upload

---
+++ application/model/model_bert.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import numpy as np
+
+from transformers import BertTokenizer, BertModel
+
+
+class Model(nn.Module):
+
+    def __init__(self, title_embedding_size=768, max_seq_len=128, output_size=2, bert_pretrained_name='bert-base-uncased'):
+        super(Model, self).__init__()
+
+        self.title_embedding = TitleEmbedding(title_embedding_size=title_embedding_size,
+                                                  max_seq_len=max_seq_len,
+                                                  bert_pretrained_name=bert_pretrained_name)
+
+        self.fc = nn.Linear(title_embedding_size, output_size)
+
+
+    def forward(self, indexed_tokens, segments_ids):
+
+        x = self.title_embedding(indexed_tokens=indexed_tokens, segments_ids=segments_ids)
+
+        x = self.fc(x)
+
+        return x
+
+# title embedding
+# 输入为title句子，无需预处理
+# 这里用bert
+class TitleEmbedding(nn.Module):
+
+    def __init__(self, title_embedding_size=768, max_seq_len=512, bert_pretrained_name='bert-base-uncased'):
+        super(TitleEmbedding, self).__init__()
+
+        self.bert = BertModel.from_pretrained(bert_pretrained_name)
+
+
+    def forward(self, indexed_tokens, segments_ids):
+        # encoded_layers.shape: (batchsize, max_seq_len, 768)
+        encoded_layers, pooler_embedding = self.bert(indexed_tokens.detach(), segments_ids.detach())
+
+        # token embedding的平均值作为sentence embedding
+        # TODO 这里可调，先用[CLS]的embedding
+        #embedding = encoded_layers.mean(dim=1)
+        embedding = encoded_layers[:, 0, :]
+
+        return embedding
---
+++ application/model/test_bert.py
@@ -0,0 +1,59 @@
+import torch
+from transformers import BertTokenizer
+
+from application.model.model_bert import Model
+
+def preprocess_sentence(sentence):
+    """处理sentence获取bert格式的输入（indexed_tokens, segments_ids）
+    """
+    # 处理title，获取索引
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    tokenized_text = tokenizer.tokenize(sentence)[:128 - 2]
+    # 补[PAD]
+    if len(tokenized_text) < 128 - 1:
+        tokenized_text.extend(['[PAD]' for _ in range(128 - len(tokenized_text) - 1)])
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+    # 这里只有单句，segments_ids全为0
+    segments_ids = [0 for _ in range(len(indexed_tokens))]
+
+    return torch.tensor(indexed_tokens), torch.tensor(segments_ids)
+
+def predict_bert(text):
+
+    checkpoint_path = "./application/model/brain/epoch_1.pth"
+    devide = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Model(title_embedding_size=768,
+                  max_seq_len=128,
+                  output_size=2,
+                  bert_pretrained_name="bert-base-uncased")
+
+    if checkpoint_path is not None:
+        model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('cpu')))
+    model.to(devide)
+
+    model.eval()
+
+    id2class = {
+        0: "fake",
+        1: "real"
+    }
+
+    with torch.no_grad():
+
+        indexed_tokens, segments_ids = preprocess_sentence(text)
+
+        indexed_tokens = indexed_tokens.unsqueeze(dim=0).to(devide)
+        segments_ids = segments_ids.unsqueeze(dim=0).to(devide)
+
+        output = model(indexed_tokens, segments_ids)
+
+        predict = output.argmax()
+        print("Input: %s" % text)
+        print(id2class[predict.item()])
+
+        return id2class[predict.item()]
+
+if __name__ == "__main__":
+    text = "WASHINGTON (Reuters) - The head of a conservative Republican faction in the U.S. Congress, who voted this month for a huge expansion of the national debt to pay for tax cuts, called himself a “fiscal conservative” on Sunday and urged budget restraint in 2018. In keeping with a sharp pivot under way among Republicans, U.S. Representative Mark Meadows, speaking on CBS’ “Face the Nation,” "
+
+    result = predict(text)
